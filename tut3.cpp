6e7270f1067a4970a1975c31a106023968ba8302
Devesh Singh <zero2march@gmail.com>
2026-06-17 21:26:01 +0530

    Update tut3.cpp

---
+++ tut3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+// this program was created by Devesh
+/*gdyedje
+dhfde e
+ djhdid
+  dghd gupe
+   djhdjh*/
+int main()
+{
+    cout << "Hello Devesh" << endl;
+    return 0;
+}
