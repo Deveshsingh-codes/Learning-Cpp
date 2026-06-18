d9f2feefd2477d142f9f6bea327115e19656d1ee
Devesh Singh <zero2march@gmail.com>
2026-06-18 21:58:08 +0530

    Update tut5.cpp

---
+++ tut5.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    cout<<"Enter the value of num1:\n"; /* THis is called insertion operator */
+    cin>>num1; /* '>>'  is called Extraction operation */
+
+     cout<<"Enter the value of num2:\n"; /* THis is called insertion operator */
+    cin>>num2; /* '>>'  is called Extraction operation */
+
+    //cout<<" the sum is "<< num1+num2;
+    cout<<" the multiple is"<< num1*num2;
+
+    return 0;
+}
