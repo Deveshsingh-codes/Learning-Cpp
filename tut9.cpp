4faf9b3c2f1dbcf9c170743afcb2bc05e58f4b36
Devesh Singh <zero2march@gmail.com>
2026-06-18 22:03:48 +0530

    update tut9.cpp

---
+++ tut9.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // cout<<"this is tutorial 9.";
+
+    int age;
+    cout << "Tell me your age" << endl;
+    cin >> age;
+
+    //******Selection control structure: if else if else ladder */
+
+    /* if((age<18)&&(age>0)){
+        cout<<"you cann't come to my party"<<endl;
+
+    }
+    else if(age==18){
+        cout<<"you need a kid pass for the party"<<endl;
+
+    }
+    else if(age<1){
+        cout<<"you are not born yet"<<endl;
+    }
+    else{
+        cout<<"you can come to the aprty"<<endl;
+
+    }*/
+
+    // Selection control structure: switch case statements
+
+    // switch (age)
+    //{
+    // case 18:
+    //     /* code */
+    //     cout<<"you are 18";
+    //     break;
+    //
+    //    case 22:
+    //    /* code */
+    //    cout<<"you are 22"<<endl;
+    //    break;
+    //
+    //    case 2:
+    //    /* code */
+    //    cout<<"you are 2"<<endl;
+    //    break;
+    //
+    //
+    //
+    //
+    // default:
+    // cout<<"No special cases"<<endl;
+    //    break;
+    //}
+    // cout<<"Done with switch case";
+    return 0;
+}
